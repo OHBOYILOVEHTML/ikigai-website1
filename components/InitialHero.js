f0addc694173ae4ee3ccfefe0ae947aedb0802b3
WalterTRPC <Zach.massarella@gmail.com>
2022-10-11 08:42:02 +0000

    10/11/2022- Final

---
+++ components/InitialHero.js
@@ -0,0 +1,12 @@
+export default function InitialHero() {
+    return (
+        <>
+            <div>
+                <div>
+                    <h1 className="text-3xl">Ikigai</h1>
+                    <h2>A conceptual project by LoveNils</h2>
+                </div>
+            </div>
+        </>
+    )
+}
