c75b1e08cf31a4b6bbc3662f55da480ddbff1832
WalterTRPC <Zach.massarella@gmail.com>
2022-10-11 08:10:19 +0000

    First Commit

---
+++ components/NavBar.js
@@ -0,0 +1,10 @@
+// James Make Edits in Here- Create the Navbar- https://tailwindcss.com/docs/installation (See this for styling details)
+
+export default function NavBar() {
+
+    return (
+        <>
+            <h1>Here lays the Navbar.</h1>
+        </>
+    )
+}
