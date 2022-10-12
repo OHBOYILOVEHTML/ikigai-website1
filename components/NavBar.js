e4c6d454953b9a1d7e4f5f24ebfab2a933305734
LOL202404 <114473801+LOL202404@users.noreply.github.com>
2022-10-12 10:12:45 +0800

    Started css + added img

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,14 +1,16 @@
 // James Make Edits in Here- Create the Navbar- https://tailwindcss.com/docs/installation (See this for styling details)
-
+import Image from 'next/image'
+import profilepic from '/components/profilepic.aspx.jpg'
 export default function NavBar() {
 
     return (
         <>
           <div>
-            <h1>Ikigai</h1>
+            <h1 className="bg-midnight">Ikigai</h1>
             <h2>Home</h2>
             <h2>About us</h2>
             <h2>Learn</h2>
+            <Image src={profilepic}/>
           </div>
         </>
     )
