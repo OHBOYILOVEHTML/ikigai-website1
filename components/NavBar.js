db4822d570aac8223885c9177bf13d7eac35c711
LOL202404 <114473801+LOL202404@users.noreply.github.com>
2022-10-12 18:09:12 +0800

    Update NavBar.js

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,22 +2,21 @@
 import Image from 'next/image'
 import profilepic from '/components/profilepic.jpeg'
 export default function NavBar() {
-
+  // <div>
+  //   <Image src={profilepic} className=""/>
+  // </div>
     return (
         <>
-          <div className="bg-purple z-1 flex flex-row font-mono">
+          <div className="bg-purple flex flex-row font-mono py-2 justify-center">
             <div className="flex flex-row">
-              <div>
-                <Image src={profilepic} className=""/>
-              </div>
-              <div>
-                <h1 className="font-bold px-5 text-2xl text-white flex flex-row py-2">Ikigai</h1>
+              <div className="py-2 flex flex-row text-white justify-start">
+                <a className="font-bold px-4 text-2xl" href="google.com">Ikigai</a>
               </div>
             </div>
-            <div className="text-white flex flex-row py-2 items-stretch ">
-              <h2 className="hover:font-bold px-10">Home</h2>
-              <h2 className="hover:font-bold px-10">About us</h2>
-              <h2 className="hover:font-bold px-10">Learn</h2>
+            <div className="text-white flex flex-row py-3 items">
+              <a className="hover:font-bold px-10 text-xl" href="google.com">Home</a>
+              <a className="hover:font-bold px-10 text-xl" href="google.com">About us</a>
+              <a className="hover:font-bold px-10 text-xl" href="google.com">Learn</a>
             </div>
           </div>
         </>
