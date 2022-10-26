3a12a2aca565419068e3ab13cc66af099242e816
LOL202404 <114473801+LOL202404@users.noreply.github.com>
2022-10-26 12:41:57 +0800

    Added blog/learn/aboutus

---
+++ components/Learn.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import InitialHero from '../components/InitialHero';
+
+export default function Blog() {
+
+    return (
+      <>
+        <div>
+          <InitialHero />
+        </div>
+      </>
+    )
+}
