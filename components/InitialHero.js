e69cd82006f2bc37dd93738ca2ab3febf6b0ab58
idontknowhowtocode <harry43804570@gmail.com>
2022-10-26 14:32:15 +0800

    added gray border to ikigai box and changed button colours + orientation

--- components/InitialHero.js
+++ components/InitialHero.js
@@ -1,11 +1,9 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
-import exampleCharity1 from '../images/exampleCharity1.jpg';
+//import exampleCharity1 from '../images/exampleCharity1.jpg';
 
 export default function InitialHero() {
 
-    const loveNilsLink = "https://www.lovenils.org/";
     var message="New blog post out now!"
     return (
         <>
@@ -14,16 +12,16 @@ export default function InitialHero() {
                         <div class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
                             <div class="mx-auto max-w-md">
                                 <div class="divide-y divide-gray-300/50">
-                                <div className='p-9 space-y-3 text-base leading-7 text-gray-900'>
-                                    <div className=""><h2>{message}</h2></div>
-                                    <h1 className="text-9xl font-bold">IKIGAI</h1>
-                                    <h2 className="text-4xl pt-5">A conceptual project by LoveNils</h2>
-                                    <div className='pt-5 flex flex-col gap-y-3 md:flex-row md:gap-x-5'>
+                                    <div className='p-9 space-y-3 text-base leading-7 text-gray-900'>
+                                        <div className=""><h2>{message}</h2></div>
+                                        <h1 className="text-9xl font-bold">IKIGAI</h1>
+                                        <h2 className="text-4xl">A conceptual project by LoveNils</h2>
+                                    </div>
+                                    <div className='pt-8 flex flex-col gap-y-3 md:flex-row md:gap-x-10 justify-center leading-7'>
                                         
-                                        <Link href={"/blog"}><button className='rounded-md py-3 px-4 border-black hover:bg-gray-200'>Explore</button></Link>
+                                        <Link href={"/blog"}><button className='font-semibold rounded-md py-3 px-4 border-black hover:bg-gray-200'>Explore</button></Link>
                                         <Link href={"/aboutus"}><button className='rounded-md px-6 bg-red-200 border-black hover:bg-red-500'>Learn more</button></Link>
                                     </div>
-                                </div>
                             </div>
                         </div>
                     </div>
