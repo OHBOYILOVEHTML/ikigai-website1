fe47460846c125a6da611659a6bcafd41eaf6213
LOL202404 <114473801+LOL202404@users.noreply.github.com>
2022-10-27 16:34:42 +0800

    Navbar works better iphones

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,16 +1,10 @@
-// James Make Edits in Here- Create the Navbar- https://tailwindcss.com/docs/installation (See this for styling details)
 import Image from "next/image";
 import Link from "next/link";
 import logo from "/public/logo.webp";
 export default function NavBar() {
-  // <div>
-  //   <Image src={profilepic} className=""/>
-  // </div>
-  //learn --> blog, videos, lessons
-  //EMPTY DIVS ARE IMPORTANT PLS DONT MAKE ME REMOVE THEM
-  // <Image className=""><\Image>
 
   //NavBar Responsiveness still not complete - please do not attempt to edit the header
+  //pr-5 makes navbar not against side of phones
   return (
     <>
       <header className="bg-white grid grid-cols-2 sm:grid-cols-3 py-2 fixed items-center w-full z-10 drop-shadow ">
@@ -20,7 +14,7 @@ export default function NavBar() {
             IKIGAI
           </a>
         </div>
-        <div className="flex flex-row gap-x-5 sm:gap-x-20 px-10 justify-center font-sans font-bold text-red-600">
+        <div className="flex flex-row gap-x-5 sm:gap-x-20 pr-5 justify-center font-sans font-bold text-red-600">
           <a className="hover:drop-shadow hover:underline text-l" href="/">
             HOME
           </a>
