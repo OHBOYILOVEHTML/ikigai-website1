0e901e81bddeaa915caa85669809bde18e24d547
idontknowhowtocode <harry43804570@gmail.com>
2022-10-27 08:30:51 +0800

    rename class ==> className

--- components/InitialHero.js
+++ components/InitialHero.js
@@ -9,9 +9,9 @@ export default function InitialHero() {
         <>
             <section className="relative flex min-h-screen flex-row justify-center py-20 overflow-hidden">
                 <div className="font-sans flex flex-col p-10 md:flex-row md:text-left">
-                        <div class="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-                            <div class="mx-auto max-w-md">
-                                <div class="divide-y divide-gray-300/50">
+                        <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+                            <div className="mx-auto max-w-md">
+                                <div className="divide-y divide-gray-300/50">
                                     <div className='p-9 space-y-3 text-base leading-7 text-gray-900'>
                                         <div className=""><h2>{message}</h2></div>
                                         <h1 className="text-9xl font-bold">IKIGAI</h1>
