df78ea82bfbd963e57bc98b6893a1ecb488bdbe1
LOL202404 <114473801+LOL202404@users.noreply.github.com>
2022-11-29 15:54:20 +0800

    add contact

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import NavBar from '../components/NavBar';
 import InitialHero from '../components/InitialHero';
 import Footer from '../components/Footer';
 import SecondHero from '../components/SecondHero';
+import ThirdHero from '../components/ThirdHero';
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
         <div>
           <InitialHero />
           <SecondHero />
+          <ThirdHero />
         </div>
         <div>
           <Footer />
