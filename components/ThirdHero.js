573a31fb4a4951ee2d10f06be495b28d33a7f6e0
WalterTRPC <Zach.massarella@gmail.com>
2022-11-29 08:11:01 +0000

    Somewhat third hero

--- components/ThirdHero.js
+++ components/ThirdHero.js
@@ -6,19 +6,21 @@ import exampleCharity1 from "../images/exampleCharity1.jpg";
 function SecondHero() {
 
   const h1Content = "Contact Us";
+  const pContent = "Reach out to us with any questions and we would be happy to help!";
   const contactNumber = "";
-  const pContent =
-    "The Ikigai Project represents our endeavour to conduct enrichment activities for underprivileged children, granting them access to educational experiences and opportunities that will greatly benefit them in the long run. The project unites passion, love and creativity through the interactive sessions carried out by our very own students. These sessions are carried out both online (such as coding) and on campus. The on-site sessions involve an in-person coding camps, a science day and a \"Learn with LEGO\" event. To execute these various classes, we require funds to purchase the necessary equipment and obtain the sufficient resources. We would be incredibly grateful if we could use this website to raise awareness!"
+  const contactEmail = "";
 
   return (
     <>
-      <div className="flex flex-col p-14 m-9 bg-white rounded-xl justify-center lg:flex-row" id="aboutus">
-        <div className="flex grid grid-cols-2">
+      <div className="flex flex-col-reverse p-14 m-9 bg-white rounded-xl justify-center lg:flex-row-reverse" id="aboutus">
+        <div className="flex flex-row-reverse gap-x-10">
           <div>
-            <h1 className="font-bold text-3xl pb-5">{h1Content}</h1>
-            <p>{pContent}</p>
+            <h1 className="font-bold text-6xl pt-2 pb-5">{h1Content}</h1>
+            <p className="text-2xl pb-3">{pContent}</p>
+            <h2 className="text-2xl pt-8">Contact Number: {contactNumber}</h2>
+            <h2 className="text-2xl">Contact Email: {contactEmail}</h2>
           </div>
-          <div className="w-1/2 justify-self-end">
+          <div className="w-1/2 justify-self-start">
             <Image src={exampleCharity1} alt="Example test Image"/>
           </div>
         </div>
